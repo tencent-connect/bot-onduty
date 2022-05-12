e94a452e6e91865c9d965bce0a3488b051be6cde
robot_nian <2660422452@qq.com>
2022-05-13 00:37:54 +0800

    修改禁言API设置timeout的方式

--- flow/operate.py
+++ flow/operate.py
@@ -1,9 +1,7 @@
-from datetime import datetime, date, timedelta
-
-from constant import config
-
 import qqbot
 from qqbot.model.mute import MuteOption
+
+from constant import config
 
 APPID = config["token"]["appid"]
 TOKEN = config["token"]["token"]
@@ -11,10 +9,10 @@ GUILD_ID = config["params"]["guildId"]
 
 
 async def mute_all():
-    mute_api = qqbot.AsyncMuteAPI(qqbot.Token(APPID, TOKEN), False).with_timeout(5)
+    mute_api = qqbot.AsyncMuteAPI(qqbot.Token(APPID, TOKEN), False, timeout=5)
     await mute_api.mute_all(GUILD_ID, MuteOption(mute_seconds=str(8 * 3600)))
 
 
 async def end_mute_all():
-    mute_api = qqbot.AsyncMuteAPI(qqbot.Token(APPID, TOKEN), False).with_timeout(5)
+    mute_api = qqbot.AsyncMuteAPI(qqbot.Token(APPID, TOKEN), False, timeout=5)
     await mute_api.mute_all(GUILD_ID, MuteOption(mute_seconds='0'))
